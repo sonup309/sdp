b49827f4a2c5f6828479b43a5003665b307eb130
sonup309 <sonupatil1922@gmail.com>
2023-03-30 18:48:48 +0530

    newfirst

--- dom.js
+++ dom.js
@@ -1,6 +1,10 @@
-var items=document.getElementsByTagName('list-group-item');
-console.log(items)
-items[2].style.backgroundColor="green";
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold'
+var input=document.querySelector('input')
+input.value='hello world'
+var seconditem=document.querySelector('.list-group-item:nth-child(2)')
+seconditem.style.color='green';
+var seconditem=document.querySelector('.list-group-item:nth-child(2)')
+seconditem.style.backgroundColor='green';
+var odd=document.querySelectorAll('li:nth-child(odd)')
+for(var i=1;i<odd.length;i++){
+    odd[1].style.backgroundColor='green'
 }
