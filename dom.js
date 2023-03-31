89a0b9d1a0b57707f0dbca5c91bae6d560701e48
sonup309 <sonupatil1922@gmail.com>
2023-03-31 12:07:02 +0530

    sonudp

--- dom.js
+++ dom.js
@@ -1,10 +1,52 @@
-var input=document.querySelector('input')
-input.value='hello world'
-var seconditem=document.querySelector('.list-group-item:nth-child(2)')
-seconditem.style.color='green';
-var seconditem=document.querySelector('.list-group-item:nth-child(2)')
-seconditem.style.backgroundColor='green';
-var odd=document.querySelectorAll('li:nth-child(odd)')
-for(var i=1;i<odd.length;i++){
-    odd[1].style.backgroundColor='green'
-}
+var itemList =document.querySelector('#items');
+//parentElement
+itemList.parentElement.style.backgroundColor='#f4f4f4';
+console.log(itemList.parentElement.parentElement);
+//parentNode
+itemList.parentNode.style.backgroundColor='#f4f4f4';
+console.log(itemList.parentNode.parentNode)
+//childnodes
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor='yellow'
+//firstchild
+console.log(itemList.firstChild);
+//firstelementchild
+console.log(itemList.firstElementChild);
+//lastchild
+console.log(itemList.lastChild);
+//lastElemetchild
+console.log(itemList.lastElementChild)
+itemList.lastElementChild.textContent='hello word'
+//nextsibling
+console.log(itemList.nextSibling);
+//nextelementsibling
+console.log(itemList.nextElementSibling);
+//previoussibling
+console.log(itemList.previousSibling);
+//previouselementsibling
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.color='green'
+//createelement
+var newDiv=document.createElement('div')
+//addclass
+newDiv.className='hello';
+//addid
+newDiv.id='hello1';
+//addattr
+newDiv.setAttribute=('title','hello div')
+//createTextNode
+var newDivText=document.createTextNode('helloworld')
+//addtext to div
+newDiv.appendChild(newDivText);
+var container=document.querySelector('header.container')
+var container=document.querySelector('header h1');
+console.log(newDiv);
+parentNode=document.getElementById('items')
+helloworldlielement=<li>hello world
+    parentNode.innerHtml="<li>hello world<li>"+parentNode.innerHtml
+
+
+
+
+
